a5d32bf55232587f2d83daa647d9cc2c945d3675
Hyuns66 <hyuns6677@gmail.com>
2022-05-17 03:28:25 +0900

    asdf

--- piCam/GPIO_pir_motor.py
+++ piCam/GPIO_pir_motor.py
@@ -82,30 +82,60 @@ def pir_detect():
             print("nooo,,,,,,,,,,,,,")
 
 def button_pressed_callback(channel):
-    faceCascade = cv2.CascadeClassifier('haarcascade_frontface.xml')
+    # faceCascade = cv2.CascadeClassifier('haarcascade_frontface.xml')
+    # cap = cv2.VideoCapture(0)
+    # cap.set(3, 640)  # set Width
+    # cap.set(4, 480)  # set Height
+    # while True:
+    #     ret, img = cap.read()
+    #     # img = cv2.flip(img, -1)  # 상하반전
+    #     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    #     faces = faceCascade.detectMultiScale(
+    #         gray,
+    #         scaleFactor=1.2,
+    #         minNeighbors=5,
+    #         minSize=(20, 20)
+    #     )
+    #     for (x, y, w, h) in faces:
+    #         cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    #         roi_gray = gray[y:y + h, x:x + w]
+    #         roi_color = img[y:y + h, x:x + w]
+    #     cv2.imshow('video', img)  # video라는 이름으로 출력
+    #     k = cv2.waitKey(30) & 0xff
+    #     if k == 27:  # press 'ESC' to quit # ESC를 누르면 종료
+    #         break
+    # cap.release()
+    # cv2.destroyAllWindows()
+    
     cap = cv2.VideoCapture(0)
-    cap.set(3, 640)  # set Width
-    cap.set(4, 480)  # set Height
-    while True:
-        ret, img = cap.read()
-        # img = cv2.flip(img, -1)  # 상하반전
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        faces = faceCascade.detectMultiScale(
-            gray,
-            scaleFactor=1.2,
-            minNeighbors=5,
-            minSize=(20, 20)
-        )
+    cap.set(3, 640)  # WIDTH
+    cap.set(4, 480)  # HEIGHT
+
+    # 얼굴 인식 캐스케이드 파일 읽는다
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontface.xml')
+
+    while (True):
+        # frame 별로 capture 한다
+        ret, frame = cap.read()
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+        # 인식된 얼굴 갯수를 출력
+        print(len(faces))
+
+        # 인식된 얼굴에 사각형을 출력한다
         for (x, y, w, h) in faces:
-            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-            roi_gray = gray[y:y + h, x:x + w]
-            roi_color = img[y:y + h, x:x + w]
-        cv2.imshow('video', img)  # video라는 이름으로 출력
-        k = cv2.waitKey(30) & 0xff
-        if k == 27:  # press 'ESC' to quit # ESC를 누르면 종료
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+        # 화면에 출력한다
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+
     cap.release()
     cv2.destroyAllWindows()
+
 
 def set_switch_interrupt():
     GPIO.add_event_detect(switch, GPIO.FALLING,
