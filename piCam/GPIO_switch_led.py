c5befaa6c1be8c926d3aec7c0f32ec3390084e18
Hyuns66 <hyuns6677@gmail.com>
2022-05-16 16:41:44 +0900

    asdf

--- piCam/GPIO_switch_led.py
+++ piCam/GPIO_switch_led.py
@@ -1,63 +1,63 @@
-import RPi.GPIO as GPIO
-from time import sleep
-
-def main():
-    led = 22
-    switch = 23
-    state = 1
-
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setwarnings(False)
-    GPIO.setup(led, GPIO.OUT)
-    GPIO.setup(switch, GPIO.IN, GPIO.PUD_UP)
-
-    try:
-        while True:
-            if GPIO.input(switch) == 0:
-                while True:
-                    if GPIO.input(switch) == 1:
-                        state = not state
-                        GPIO.output(led, state)
-                        sleep(0.2)
-                        break
-
-    finally:
-        print("end")
-
-if __name__ == "__main__":
-    main()
-
-
-
 # import RPi.GPIO as GPIO
-# import time
+# from time import sleep
 #
-# GPIO.setmode(GPIO.BCM)
-# switch = 23
-# led = 24
-# print("start")
+# def main():
+#     led = 22
+#     switch = 23
+#     state = 1
 #
-# GPIO.setup(led, GPIO.OUT)
-# GPIO.setup(switch, GPIO.IN)
+#     GPIO.setmode(GPIO.BCM)
+#     GPIO.setwarnings(False)
+#     GPIO.setup(led, GPIO.OUT)
+#     GPIO.setup(switch, GPIO.IN, GPIO.PUD_UP)
 #
-# try:
-#     while True:
-#     StartTime = time.time()
-#     StopTime = time.time()
-#     GPIO.output(led, True)
-#     time.sleep(0.00001)
-#     GPIO.output(led, False)
+#     try:
+#         while True:
+#             if GPIO.input(switch) == 0:
+#                 while True:
+#                     if GPIO.input(switch) == 1:
+#                         state = not state
+#                         GPIO.output(led, state)
+#                         sleep(0.2)
+#                         break
 #
-#     while GPIO.input(switch) == 0:
-#         StartTime = time.time()
+#     finally:
+#         print("end")
 #
-#     while GPIO.input(switch) == 1:
-#         StartTime = time.time()
-#
-#     TimeElapsed = StopTime - StartTime
-#     distnace = round((TimeElapsed * 34300) / 2, 2)
-#     print("Distance = ", distance, "cm")
-#     time.sleep(1)
-#
-# except KeyboardInterrupt:
-#     pass
+# if __name__ == "__main__":
+#     main()
+
+
+
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+sensor = 22
+led = 23
+print("start")
+
+GPIO.setup(led, GPIO.OUT)
+GPIO.setup(switch, GPIO.IN)
+
+try:
+    while True:
+    StartTime = time.time()
+    StopTime = time.time()
+    GPIO.output(led, True)
+    time.sleep(0.00001)
+    GPIO.output(led, False)
+
+    while GPIO.input(switch) == 0:
+        StartTime = time.time()
+
+    while GPIO.input(switch) == 1:
+        StartTime = time.time()
+
+    TimeElapsed = StopTime - StartTime
+    distnace = round((TimeElapsed * 34300) / 2, 2)
+    print("Distance = ", distance, "cm")
+    time.sleep(1)
+
+except KeyboardInterrupt:
+    pass
